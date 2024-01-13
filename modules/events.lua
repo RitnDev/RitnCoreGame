50cfb7b38132746b9096611111851c6e5089f464
RitnDev <ritn@hotmail.fr>
2024-01-13 20:35:31 +0100

    0.3.3 - fix : on_configuration_changed (RitnForce) data_force non chargé

--- modules/events.lua
+++ modules/events.lua
@@ -33,8 +33,29 @@ local function on_init_mod()
 
     log('on_init : RitnCoreGame -> finish !')
 end
+
+
+local function on_configuration_changed()
+    log('RitnCoreGame -> on_configuration_changed')
+    ---------------------------------
+    global.core.cheatModeActivated = false
+    global.core.multiplayer = game.is_multiplayer()
+    ---------------------------------
+    remote.call("RitnCoreGame", "init_data", "force", {
+        index = 0,
+        name = "",
+        exception = true,
+        players = {},
+        force_used = false,
+        finish = false,
+    })
+    remote.call("RitnCoreGame", "init_data", "force_player", { name = ""})
+    log('on_configuration_changed : RitnCoreGame -> finish !')
+end
+
 ------------------------------------------------------------------------------------------------------------
 -- events :
 script.on_init(on_init_mod)
+script.on_configuration_changed(on_configuration_changed)
 ---------------------------------------------------------------------------------------------
 return {}
